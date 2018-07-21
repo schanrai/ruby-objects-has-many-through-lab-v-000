763d4072ae81b3f909d5e8c11c22ca91a419d641
Sushi Chanrai <sushi@pathworks.co>
2018-07-21 15:11:55 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -2,10 +2,10 @@ class Patient
   attr_accessor :name
   @@all = []
 
-  #def initialize(name)
-    #@name = name
-  #  @@all << self
-  #end
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
 
   def self.all
     @@all
