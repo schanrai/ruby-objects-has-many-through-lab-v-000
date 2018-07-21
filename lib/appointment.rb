897ac71750fc215b478a9d3e0839dda5a96e4ba0
Sushi Chanrai <sushi@pathworks.co>
2018-07-21 14:59:10 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,6 +1,17 @@
 class Appointment
-  attr_accessor :name, :doctor, :date
+  attr_accessor :name, :doctor, :date, :patient
   @@all = []
+
+  def initialize(patient, doctor, date)
+    @patient = patient
+    @doctor = doctor
+    @date = date
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
 
 
