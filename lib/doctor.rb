8c2882c82350c9cf2228f62a4911f55772713bec
Sushi Chanrai <sushi@pathworks.co>
2018-07-21 15:34:21 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -14,7 +14,7 @@ class Doctor
   end
 
   def new_appointment(patient, date)
-    new_appointment = Appointment.new(patient, self, date)
+    Appointment.new(patient, self, date)
   end
 
   def appointments
