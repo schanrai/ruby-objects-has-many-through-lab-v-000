387e4918debdf277def59d5fce5264497ec60003
Sushi Chanrai <sushi@pathworks.co>
2018-07-21 14:55:32 +0000

    automatically backed up by learn

---
+++ lib/appointment.rb
@@ -0,0 +1,13 @@
+class Appointment
+  attr_accessor :name
+  @@all = []
+end
+
+
+=begin
+
+* The `Appointment` class needs a class variable `@@all` that begins as an empty array.
+* The `Appointment` class needs a class method `.all` that lists each artist in the class variable.
+* An appointment should be initialized with a date (as a string, like `"Monday, August 1st"`), a patient, and a doctor. The appointment should be saved in the `@@all` array.
+
+=end
---
+++ lib/patient.rb
@@ -0,0 +1,17 @@
+class Patient
+  attr_accessor :name
+  @@all = []
+
+end
+
+
+=begin
+
+* The `Patient` class needs a class variable `@@all` that begins as an empty array.
+* The `Patient` class needs a class method `.all` that lists each artist in the class variable.
+* A patient is instantiated with a name and be saved in the `@@all` array.
+* The `Patient` class needs an instance method, `#new_appointment`, that takes in an argument of a doctor and a date and creates a new appointment. The appointment should know that it belongs to the patient.
+* The `Patient` class needs an instance method, `#appointments`, that iterates through the appointments array and returns appointments that belong to the patient.
+* The `Patient` class needs an instance method, `#doctors`, that iterates over that patient's appointments and collects the doctor that belongs to each appointment.
+
+=end
